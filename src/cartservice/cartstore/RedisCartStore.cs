72552b66167696197aa22a953c4feb9aec98e90e
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-05-31 16:37:16 +0200

    [cartservice] switch to Splunk .NET OTel distribution (#125)

--- src/cartservice/cartstore/RedisCartStore.cs
+++ src/cartservice/cartstore/RedisCartStore.cs
@@ -13,14 +13,13 @@
 // limitations under the License.
 
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using cartservice.interfaces;
 using Google.Protobuf;
 using Grpc.Core;
-using OpenTracing;
-using OpenTracing.Util;
 using StackExchange.Redis;
 
 namespace cartservice.cartstore
@@ -36,18 +35,11 @@ namespace cartservice.cartstore
     private readonly byte[] emptyCartBytes;
 
     private static double EXTERNAL_DB_ACCESS_RATE = Convert.ToDouble(Environment.GetEnvironmentVariable("EXTERNAL_DB_ACCESS_RATE"));
-    private static Int16 EXTERNAL_DB_MAX_DURATION_MILLIS = Convert.ToInt16(Environment.GetEnvironmentVariable("EXTERNAL_DB_MAX_DURATION_MILLIS"));
+    private static short EXTERNAL_DB_MAX_DURATION_MILLIS = Convert.ToInt16(Environment.GetEnvironmentVariable("EXTERNAL_DB_MAX_DURATION_MILLIS"));
     private static double EXTERNAL_DB_ERROR_RATE = Convert.ToDouble(Environment.GetEnvironmentVariable("EXTERNAL_DB_ERROR_RATE"));
 
     public static string EXTERNAL_DB_NAME = Environment.GetEnvironmentVariable("EXTERNAL_DB_NAME") ?? "global.datastore";
 
-    public static string CACHE_HIT_REDIS_ERROR = Environment.GetEnvironmentVariable("CACHE_HIT_REDIS_ERROR") ?? "true";
-    public static string SCAN_REDIS_ERROR = Environment.GetEnvironmentVariable("SCAN_REDIS_ERROR") ?? "true";
-    private static Int16 FORCED_REDIS_SCAN_COUNT = Convert.ToInt16(Environment.GetEnvironmentVariable("FORCED_REDIS_SCAN_COUNT"));
-
-     public static string REDIS_ADDR = Environment.GetEnvironmentVariable("REDIS_ADDR") ?? "False";
-   
-    private readonly ITracer _tracer;
     private readonly Random _random;
 
     public RedisCartStore(ConnectionMultiplexer connection)
@@ -57,9 +49,8 @@ namespace cartservice.cartstore
       var cart = new Hipstershop.Cart();
       emptyCartBytes = cart.ToByteArray();
 
-      _tracer = GlobalTracer.Instance;
-      _random = new Random();
-
+      _random = Random.Shared;
+      _dbCache = new DatabaseCache(connection);
     }
 
     public Task InitializeAsync()
@@ -216,76 +207,39 @@ namespace cartservice.cartstore
 
     public bool Ping()
     {
-        try
-        {
-          var cache = redis.GetDatabase();
-          var res = cache.Ping();
-          
-          if (SCAN_REDIS_ERROR == "false" || SCAN_REDIS_ERROR == "False") // If we pass True forFORCE_REDIS_ERROR we are going to Create extra lopad on the DB
-          {
-            Console.WriteLine($"going to create extra load on redis server");
-            for (int i=0; i <= MyGlobals.Total; i++)
-            {
-              //redis.GetServer(REDIS_ADDR).Keys(); 
-              var server = redis.GetServer(REDIS_ADDR); 
-                    
-              foreach (var key in server.Keys())
-              {
-                Console.WriteLine(key);
-              } 
-              
-          
-            } 
-            Console.WriteLine($"Ping called with i={MyGlobals.Total}");
-            //Thread.Sleep(_random.Next(0, EXTERNAL_DB_MAX_DURATION_MILLIS));
-            Thread.Sleep(1000);
-            
-            if (FORCED_REDIS_SCAN_COUNT <= 0)  
-            {
-              FORCED_REDIS_SCAN_COUNT = 150;
-            } 
-            if (MyGlobals.Total <= FORCED_REDIS_SCAN_COUNT) 
-            {
-              MyGlobals.Total = MyGlobals.Total + 1; 
-            }
-            else
-            {
-              MyGlobals.Total = 1;
-            }   
-          }
-          var values = new NameValueEntry[]
-          {
-              new NameValueEntry("sensor_id", "56"),
-              new NameValueEntry("temp", "2")
-          };
+      try
+      {
+        var db = _dbCache.ByPassBlocking();
+        var res = db.Ping();
+        return res != TimeSpan.Zero;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
 
+    private async Task<bool> ConditionallyMockExternalResourceAccess(string operation)
+    {
+      if (_random.NextDouble() >= EXTERNAL_DB_ACCESS_RATE)
+      {
+        return false;
+      }
 
-          var messageId = cache.StreamAdd("sensor_stream", values);
+      using var activity = ActivitySourceUtil.ActivitySource.StartActivity(operation, ActivityKind.Client);
 
-          var streams = cache.StreamRead(new StreamPosition[]
-          {
-              new StreamPosition("events_stream", "0-0"),
-              new StreamPosition("score_stream", "0-0")
-          });
-          string someKey = cache.KeyRandom();
-          using (IScope scope = _tracer.BuildSpan("Cart.DbQuery.ping").WithTag("span.kind", "client").StartActive())
-          {
-            string  db_Statement = "";
-            ISpan span = scope.Span;
-            db_Statement = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ($1)";
-            span.SetTag("db.system", "mysql"); 
-            span.SetTag( "db.statement",db_Statement);
-            span.SetTag("peer.service", "mysql:LxvGChW075"); 
+      activity?.SetTag("db.system", "postgres");
+      activity?.SetTag("db.type", "postgres");
+      activity?.SetTag("peer.service", EXTERNAL_DB_NAME + ":98321");
 
-            Console.WriteLine($"local dababase using statement={db_Statement}" );
-            
-          }     
-          return res != TimeSpan.Zero;
-        }
-        catch (Exception)
-        {
-          return false;
-        }
+      if (_random.NextDouble() < EXTERNAL_DB_ERROR_RATE)
+      {
+          activity?.SetStatus(ActivityStatusCode.Error);
+      }
+
+      await Task.Delay(_random.Next(0, EXTERNAL_DB_MAX_DURATION_MILLIS));
+
+      return true;
     }
   }
 
